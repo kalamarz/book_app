897b9498d4dddf52070cc34ca6a2fed10b26f44e
Magdalena Kałamarz <magda.kalamarz@wp.pl>
2019-03-31 13:22:58 +0200

    Change BooksList component. Fetch books from database

--- src/components/books/BooksList.js
+++ src/components/books/BooksList.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class BooksList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/books/')
+      .then(res => {
+        this.setState({
+          books: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
   onDeleteClick = ( bookId) => { 
 
   }
 
   render() {
-    const { books } = this.props;
+    const { books } = this.state;
 
     if(!books) return <p className='loading'>Loading...</p>;
     return (
@@ -28,14 +45,14 @@ class BooksList extends Component {
           </thead>
           <tbody>
             { books.map(book => (
-            <tr key={book.id}>
+            <tr key={book._id}>
               <td>{book.title}</td>
               <td>{book.author}</td>
               <td>{book.genre}</td>
               <td>{book.year}</td>
               <td>{book.pages}</td>
               <td>{book.isbn}</td>
-              <td><button className='button button--edit'><Link to={`/edit/${book.id}`}>Edit</Link></button></td>
+              <td><button className='button button--edit'><Link to={`/edit/${book._id}`}>Edit</Link></button></td>
               <td><button className='button button--delete' onClick={ () => this.onDeleteClick(book.id)}>Delete</button></td>
             </tr>
             ))}
