b1249957ea4240ad99e2aba3f3fb28691b0e99da
Magdalena Kałamarz <magda.kalamarz@wp.pl>
2019-04-09 19:31:54 +0200

    Add build

---
+++ client/build/precache-manifest.d508711b6b9327a528d17aa839a6863e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cfdc272aa1f9749d03b",
+    "url": "/static/js/main.6cfdc272.chunk.js"
+  },
+  {
+    "revision": "47f45e7106e836d0e6a7",
+    "url": "/static/js/1.47f45e71.chunk.js"
+  },
+  {
+    "revision": "6cfdc272aa1f9749d03b",
+    "url": "/static/css/main.ef71647a.chunk.css"
+  },
+  {
+    "revision": "4ad19b08b14bfb1889615fd095b50ed9",
+    "url": "/index.html"
+  }
+];
